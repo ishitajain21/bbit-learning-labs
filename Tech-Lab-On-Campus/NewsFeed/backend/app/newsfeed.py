1b37bf12344f524610f31e192e7acdc551bb6dbf
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2025-04-03 18:37:07 -0700

    hello

--- Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
+++ Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-
+# POST: pay , PUT: phone number
 @dataclass
 class Article:
     """Dataclass for an article."""
@@ -14,18 +14,40 @@ class Article:
     publish_date: datetime
     image_url: str
     url: str
+    
 
-
+def format_article(article: dict) -> Article:
+    """Format a dictionary into an Article object."""
+    return Article(
+        author=article['author'],
+        title=article['title'],
+        body=article['body'],
+        publish_date=datetime.strptime(article['publish_date'], '%Y-%m-%d'),
+        image_url=article['image_url'],
+        url=article['url']
+    )
 def get_all_news() -> list[Article]:
     """Get all news articles from the datastore."""
     # 1. Use Redis client to fetch all articles
+    RedisClient = RedisClient()
+    entrie = RedisClient.get_entry('all_articles')
     # 2. Format the data into articles
+    entries = []
+    for i in entrie:
+        entries.append(format_article(i))
+        
     # 3. Return a list of the articles formatted 
-    return []
+    return [entries]
 
 
 def get_featured_news() -> Article | None:
     """Get the featured news article from the datastore."""
     # 1. Get all the articles
+    RedisClient = RedisClient()
+    entrie = RedisClient.get_entry('all_articles')
+    l = []
+    for i in entrie:
+        l.append(format_article(i))
+    l.sort(key=lambda x: x.publish_date, reverse=True)
     # 2. Return as a list of articles sorted by most recent date
-    return None
+    return l
